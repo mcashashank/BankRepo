9a366a016cf88beca92a7f929e453cb1cf70beef
raja <Shashank@TheDevil>
2017-08-24 22:59:29 +0530

    tl keep project

---
+++ SumProj/src/com/nt/operation/SumOperation.java
@@ -0,0 +1,8 @@
+package com.nt.operation;
+
+public class SumOperation {
+
+	public int sum(int a,int b) {
+		return a+b;
+	}
+}
---
+++ SumProj/src/com/nt/test/TestMain.java
@@ -0,0 +1,16 @@
+package com.nt.test;
+
+import com.nt.operation.*;
+public class TestMain {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=90;
+		
+		SumOperation op=new SumOperation();    
+		System.out.println(op.sum(a, b));
+		
+	}
+
+}
